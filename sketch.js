99907acdffddeffbf7920d82859f99f3e04bc881
Victor Eduardo Borges Saraiva <141414307+vitim1a@users.noreply.github.com>
2023-08-11 08:18:28 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(600, 600);
+  background("black");
+}
+
+  function draw() {
+  
+ 
+stroke("white"); 
+  fill('gold');   
+      
+    
+    if(mouseIsPressed){
+      rect(mouseX,mouseY,30,30)
+    }
+  
+  
+  
+  
+}
